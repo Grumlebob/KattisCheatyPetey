9431266a62121b071c5714e7ae6cf89ad734609c
Thomas Sørup Bavnhøj <tbav@itu.dk>
2023-04-17 13:16:37 +0200

    groups

--- data/generate.sh
+++ data/generate.sh
@@ -18,6 +18,7 @@ compile gen_Specific_case.py
 
 
 # there are two *.in files in data/sample
+samplegroup
 sample 1
 sample 2
 sample 3
@@ -25,13 +26,25 @@ sample 4
 
 # generate tests here
 #tc 1 # data/sample/2.in belongs here
+
+group even 25
+tc evenRulecard gen_even_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
+
+
+group odd 25
+tc oddRulecard gen_odd_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
+
+
+group four 25
+tc FiveRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 5
+
+
+group two 25
+
 tc smallNumbers1 gen_random --loRandom 1 --hiRandom 2 --loCards 1 --hiCards 5
 tc smallNumbers2 gen_random --loRandom 1 --hiRandom 10 --loCards 1 --hiCards 21
 tc highNumbers1 gen_random --loRandom 1 --hiRandom 5000 --loCards 1 --hiCards 21
 tc highNumbers2 gen_random --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-tc evenRulecard gen_even_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-tc oddRulecard gen_odd_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-tc FiveRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 5
 tc FiveRulecardImpossibleToHit gen_Specific_rulecard --loCards 1 --hiCards 3 --ruleCard 5
 tc ThreeRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 3
 tc CatchesGreedyAlgorithm gen_Specific_case --ruleCard 1 --cardsAvailable 1 5 8 17
