08a01d94c5c661f1921b1981729fee1c2b0fabcf
Thomas Sørup Bavnhøj <tbav@itu.dk>
2023-04-17 14:42:07 +0200

    Revert groupings

--- data/generate.sh
+++ data/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USE_SCORING=100 #Pass-fail problem.
+USE_SCORING=0 #Pass-fail problem.
 # run this as ./generate.sh from the data directory. It populates data/secret
 
 . ../../_testdata_tools/gen.sh
@@ -17,7 +17,6 @@ compile gen_Specific_case.py
 
 
 # there are two *.in files in data/sample
-samplegroup
 sample 1
 sample 2
 sample 3
@@ -26,28 +25,23 @@ sample 4
 # generate tests here
 #tc 1 # data/sample/2.in belongs here
 
-group even 20
 tc evenRulecard gen_even_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
 tc EvenRulecardImpossible gen_Specific_case --ruleCard 2 --cardsAvailable 5
 
 
-group odd 20
 tc oddRulecard gen_odd_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
 tc CatchesGreedyAlgorithm gen_Specific_case --ruleCard 1 --cardsAvailable 1 5 8 17
 
 
-group three 20
 tc ruleCard3Impossible gen_Specific_case --ruleCard 3 --cardsAvailable 5 10 15 20
 tc ThreeRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 3
 
 
-group five 20
 tc FiveRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 5
 tc FiveRulecardImpossibleToHit gen_Specific_rulecard --loCards 1 --hiCards 3 --ruleCard 5
 tc ruleCard5Impossible gen_Specific_case --ruleCard 5 --cardsAvailable 1 2 3 4
 
 
-group all 20
 tc smallNumbers1 gen_random --loRandom 1 --hiRandom 2 --loCards 1 --hiCards 5
 tc smallNumbers2 gen_random --loRandom 1 --hiRandom 10 --loCards 1 --hiCards 21
 tc highNumbers1 gen_random --loRandom 1 --hiRandom 5000 --loCards 1 --hiCards 21
