1e09b39b06b852670c986410c19de053895d6240
AGmarson <tcla@itu.dk>
2023-04-17 14:47:03 +0200

    fixed some names and added threeImpossibleToHit

--- data/generate.sh
+++ data/generate.sh
@@ -28,23 +28,25 @@ sample 4
 
 group even 20
 tc evenRulecard gen_even_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-tc EvenRulecardImpossible gen_Specific_case --ruleCard 2 --cardsAvailable 5
+tc evenRulecardImpossible gen_Specific_case --ruleCard 2 --cardsAvailable 5
 
 
 group odd 20
 tc oddRulecard gen_odd_rulecard --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-tc CatchesGreedyAlgorithm gen_Specific_case --ruleCard 1 --cardsAvailable 1 5 8 17
+tc catchesGreedyAlgorithm gen_Specific_case --ruleCard 1 --cardsAvailable 1 5 8 17
 
 
 group three 20
-tc ruleCard3Impossible gen_Specific_case --ruleCard 3 --cardsAvailable 5 10 15 20
-tc ThreeRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 3
+tc threeRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 3
+tc threeRulecardImpossibleToHit gen_Specific_rulecard --loCards 1 --hiCards 10 --ruleCard 3
+tc threeRuleCardImpossible gen_Specific_case --ruleCard 3 --cardsAvailable 5 10 15 20
 
 
 group five 20
-tc FiveRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 5
-tc FiveRulecardImpossibleToHit gen_Specific_rulecard --loCards 1 --hiCards 3 --ruleCard 5
-tc ruleCard5Impossible gen_Specific_case --ruleCard 5 --cardsAvailable 1 2 3 4
+tc fiveRulecard gen_Specific_rulecard --loCards 1 --hiCards 21 --ruleCard 5
+tc fiveRulecardImpossibleToHit gen_Specific_rulecard --loCards 1 --hiCards 3 --ruleCard 5
+tc fiveRuleCardImpossible gen_Specific_case --ruleCard 5 --cardsAvailable 1 2 3 4
+tc fiveRuleCardPossible gen_Specific_case --ruleCard 5 --cardsAvailable 18 8 5
 
 
 group all 20
@@ -52,7 +54,3 @@ tc smallNumbers1 gen_random --loRandom 1 --hiRandom 2 --loCards 1 --hiCards 5
 tc smallNumbers2 gen_random --loRandom 1 --hiRandom 10 --loCards 1 --hiCards 21
 tc highNumbers1 gen_random --loRandom 1 --hiRandom 5000 --loCards 1 --hiCards 21
 tc highNumbers2 gen_random --loRandom 1 --hiRandom 1000000 --loCards 1 --hiCards 21
-
-
-
-
