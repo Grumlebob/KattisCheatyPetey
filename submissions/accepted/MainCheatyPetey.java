a6dcaa52686e2235c23da611aac2e64057b2bf94
AGmarson <tcla@itu.dk>
2023-05-02 11:43:49 +0200

    refactor main and add 6 rule

--- submissions/accepted/MainCheatyPetey.java
+++ submissions/accepted/MainCheatyPetey.java
@@ -1,10 +1,6 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
-
-import static java.lang.Math.abs;
-import static java.lang.Math.min;
 
 public class MainCheatyPetey {
     private static class MultiSet<T> implements Iterable<T>, Cloneable {
@@ -44,13 +40,7 @@ public class MainCheatyPetey {
 
         @Override
         public Iterator<T> iterator() {
-            var list = new ArrayList<T>();
-            for (T obj : ms.keySet()) {
-                for (int i = numberOf(obj); i > 0; i--) {
-                    list.add(obj);
-                }
-            }
-            return list.iterator();
+            return ms.keySet().iterator();
         }
 
         @Override
@@ -58,6 +48,45 @@ public class MainCheatyPetey {
         public MultiSet<T> clone() {
             return new MultiSet<T>((HashMap<T, Integer>) ms.clone());
         }
+    }
+    
+    private static int bottumUp(MultiSet<Integer> availableValues, int target, boolean worst, boolean oneOfEach) {
+        int[] dp = new int[target + 1];
+        @SuppressWarnings("unchecked")
+        MultiSet<Integer>[] valuesAvailableFrom = new MultiSet[target + 1];; //only used if oneOfEach == true
+        for (var i = 0; i < valuesAvailableFrom.length && oneOfEach; i++) {
+            valuesAvailableFrom[i] = availableValues.clone();
+        }
+        dp[0] = 0;
+        for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
+            // Initialize every current best solution to Max value, as that is worse than
+            // any other solution.
+            // When finding minimum we set this number high, if we are finding max we set it
+            // low.
+            // Because we want to find something where they draw the most cards, and drawing
+            // 1 card is more than 0.
+            dp[currentTarget] = worst ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int currentCardValue : availableValues) {
+                // If we don't overdraw.
+                if (currentTarget - currentCardValue < 0 || dp[currentTarget - currentCardValue] == Integer.MAX_VALUE || dp[currentTarget - currentCardValue] == Integer.MIN_VALUE) {
+                    continue;
+                }
+                // System.out.println("Current target:" + currentTarget + " Current card value:"
+                // + currentCardValue
+                // + " Current best solution:" + dp[currentTarget] + " New solution:"
+                // + dp[currentTarget - currentCardValue] + 1);
+                if (worst && dp[currentTarget] < dp[currentTarget - currentCardValue] + 1 || !worst && dp[currentTarget] > dp[currentTarget - currentCardValue] + 1) {
+                    dp[currentTarget] = dp[currentTarget - currentCardValue] + 1;
+                    if (oneOfEach) {
+                        valuesAvailableFrom[currentTarget] = valuesAvailableFrom[currentTarget - currentCardValue].clone();
+                        valuesAvailableFrom[currentTarget].remove(currentCardValue);
+                    }
+                }
+                // System.out.println(dp[currentTarget]);
+            }
+        }
+
+        return dp[target];
     }
 
     public static void main(String[] args) {
@@ -79,230 +108,44 @@ public class MainCheatyPetey {
         // System.out.println("valuesAvailable: " + valuesAvailable);
 
         // Dynamic programming coin change problem variant, casino themed.
-        int[] dp = new int[target + 1];
-
-        if (ruleCard % 2 == 0) {
-            // EVEN: highest amount of cards
-            dp[0] = 0;
-            for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
-                // Initialize every current best solution to Max value, as that is worse than
-                // any other solution.
-                // When finding minimum we set this number high, if we are finding max we set it
-                // low.
-                // Because we want to find something where they draw the most cards, and drawing
-                // 1 card is more than 0.
-                dp[currentTarget] = 0;
-                for (int currentCardValue : valuesAvailable) {
-                    // If we don't overdraw.
-                    if (currentTarget - currentCardValue >= 0 && currentTarget + currentCardValue <= target + 1) {
-                        // System.out.println("Current target:" + currentTarget + " Current card value:"
-                        // + currentCardValue
-                        // + " Current best solution:" + dp[currentTarget] + " New solution:"
-                        // + dp[currentTarget - currentCardValue] + 1);
-                        dp[currentTarget] = Math.max(dp[currentTarget], dp[currentTarget - currentCardValue] + 1);
-                        // System.out.println(dp[currentTarget]);
-                    }
-                }
-            }
-            // Lowest card is 1, so if dp[target] is above our target, we can't reach it, as
-            // value was never changed from MAX_VALUE.
-            if (abs(dp[target]) == 0) {
-                System.out.println("Impossible");
-            } else {
-                System.out.println(dp[target]);
-                // Print the different cards used
-                // System.out.println("Cards used:");
-                // int x = target;
-                // while (x > 0) {
-                // for (var c : valuesAvailable) {
-                // if (x - c >= 0 && dp[x] == dp[x - c] + 1) {
-                // System.out.println(c);
-                // x -= c;
-                // break;
-                // }
-                // }
-                // }
-            }
-
-        } else if (ruleCard == 5)
-        // Only 5 of each card
-        {
-            @SuppressWarnings("unchecked") // java doesn't like arrays of generics
-            MultiSet<Integer>[] valuesAvailableFrom = new MultiSet[dp.length];
-            for (int i = 0; i < valuesAvailableFrom.length; i++) {
-                var aux = new MultiSet<Integer>();
-                for (var val : valuesAvailable) {
-                    aux.add(val, 5);
-                }
-                valuesAvailableFrom[i] = aux; // for target i you have x cards to pick from
-            }
-            dp[0] = 0;
-            for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
-                // Initialize every current best solution to Max value, as that is clearly worse
-                // than any other solution.
-                dp[currentTarget] = target + 99999;
-                for (int currentCardValue : valuesAvailable) {
-                    // If we don't overdraw.
-                    if (currentTarget - currentCardValue >= 0) {
-                        // System.out.println("Current target:" + currentTarget + " Current card value:"
-                        // + currentCardValue + " Current best solution:" + dp[currentTarget] + " New
-                        // solution:" + dp[currentTarget-currentCardValue]+1);
-                        // The best solution is the one with fewest amount of cards drawn.
-                        // We are doing bottom-up, checking every possible solution from 1 all the way
-                        // to our target.
-                        // If our previous solution, plus drawing an extra card, is better than our
-                        // current best solution, we update it.
-                        if (dp[currentTarget - currentCardValue] + 1 < dp[currentTarget]
-                                && valuesAvailableFrom[currentTarget - currentCardValue]
-                                        .numberOf(currentCardValue) > 0) {
-                            valuesAvailableFrom[currentTarget] = valuesAvailableFrom[currentTarget - currentCardValue]
-                                    .clone();
-                            valuesAvailableFrom[currentTarget].remove(currentCardValue);
-                            dp[currentTarget] = dp[currentTarget - currentCardValue] + 1;
-                        } else {
-                            dp[currentTarget] = dp[currentTarget];
-                        }
-                    }
-                }
-            }
-            // Lowest card is 1, so if dp[target] is above our target, we can't reach it, as
-            // value was never changed from MAX_VALUE.
-            if (abs(dp[target]) > target) {
-                System.out.println("Impossible");
-            } else {
-                // System.out.println("Minimum cards:");
-                System.out.println(dp[target]);
-                //// Print the different cards used
-                // System.out.println("Cards used:");
-                // int x = target;
-                // while (x > 0) {
-                // for (var c : valuesAvailable) {
-                // if (x - c >= 0 && dp[x] == dp[x - c] + 1) {
-                // System.out.println(c);
-                // x -= c;
-                // break;
-                // }
-                // }
-                // }
-            }
-        } else if (ruleCard == 3)
+        
         // Only 1 of each card
-        {
-            @SuppressWarnings("unchecked") // java doesn't like arrays of generics
-            MultiSet<Integer>[] valuesAvailableFrom = new MultiSet[dp.length];
-            for (int i = 0; i < valuesAvailableFrom.length; i++) {
-                var aux = new MultiSet<Integer>();
-                for (var val : valuesAvailable) {
-                    aux.add(val, 1);
-                }
-                valuesAvailableFrom[i] = aux; // for target i you have x cards to pick from
-            }
-            dp[0] = 0;
-            for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
-                // Initialize every current best solution to Max value, as that is clearly worse
-                // than any other solution.
-                dp[currentTarget] = target + 99999;
-                for (int currentCardValue : valuesAvailable) {
-                    // If we don't overdraw.
-                    if (currentTarget - currentCardValue >= 0) {
-                        // System.out.println("Current target:" + currentTarget + " Current card value:"
-                        // + currentCardValue + " Current best solution:" + dp[currentTarget] + " New
-                        // solution:" + dp[currentTarget-currentCardValue]+1);
-                        // The best solution is the one with fewest amount of cards drawn.
-                        // We are doing bottom-up, checking every possible solution from 1 all the way
-                        // to our target.
-                        // If our previous solution, plus drawing an extra card, is better than our
-                        // current best solution, we update it.
-                        if (dp[currentTarget - currentCardValue] + 1 < dp[currentTarget]
-                                && valuesAvailableFrom[currentTarget - currentCardValue]
-                                        .numberOf(currentCardValue) > 0) {
-                            valuesAvailableFrom[currentTarget] = valuesAvailableFrom[currentTarget - currentCardValue]
-                                    .clone();
-                            valuesAvailableFrom[currentTarget].remove(currentCardValue);
-                            dp[currentTarget] = dp[currentTarget - currentCardValue] + 1;
-                        } else {
-                            dp[currentTarget] = dp[currentTarget];
-                        }
-                    }
-                }
-            }
-            // Lowest card is 1, so if dp[target] is above our target, we can't reach it, as
-            // value was never changed from MAX_VALUE.
-            if (abs(dp[target]) > target) {
-                System.out.println("Impossible");
-            } else {
-                // System.out.println("Minimum cards:");
-                System.out.println(dp[target]);
-                //// Print the different cards used
-                // System.out.println("Cards used:");
-                // int x = target;
-                // while (x > 0) {
-                // for (var c : valuesAvailable) {
-                // if (x - c >= 0 && dp[x] == dp[x - c] + 1) {
-                // System.out.println(c);
-                // x -= c;
-                // break;
-                // }
-                // }
-                // }
-            }
+        if (ruleCard == 3) {
+            var result = bottumUp(valuesAvailable, target, false, true);
+            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
         }
 
+        // Only 5 of each card 
+        else if (ruleCard == 5) {
+            for (var value : valuesAvailable) {
+                valuesAvailable.add(value, 4);
+            }
+            var result = bottumUp(valuesAvailable, target, false, true);
+            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
+        }
+
+        // Only 6 of each card and worst possible
+        else if (ruleCard == 6) {
+            for (var value : valuesAvailable) {
+                valuesAvailable.add(value, 5);
+            }
+            var result = bottumUp(valuesAvailable, target, true, true);
+            System.out.println(result == Integer.MIN_VALUE ?  "Impossible" : result);
+        }
+
+        // ODD: lowest amount of cards
         else if (ruleCard % 2 == 1) {
-            // ODD: lowest amount of cards
-            dp[0] = 0;
-            for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
-                // Initialize every current best solution to Max value, as that is clearly worse
-                // than any other solution.
-                dp[currentTarget] = target + 99999;
-                for (int currentCardValue : valuesAvailable) {
-                    // If we don't overdraw.
-                    if (currentTarget - currentCardValue >= 0) {
-                        // System.out.println("Current target:" + currentTarget + " Current card value:"
-                        // + currentCardValue + " Current best solution:" + dp[currentTarget] + " New
-                        // solution:" + dp[currentTarget-currentCardValue]+1);
-                        // The best solution is the one with fewest amount of cards drawn.
-                        // We are doing bottom-up, checking every possible solution from 1 all the way
-                        // to our target.
-                        // If our previous solution, plus drawing an extra card, is better than our
-                        // current best solution, we update it.
-                        dp[currentTarget] = min(dp[currentTarget], dp[currentTarget - currentCardValue] + 1);
-                    }
-                }
-            }
-
-            // Lowest card is 1, so if dp[target] is above our target, we can't reach it, as
-            // value was never changed from MAX_VALUE.
-            if (abs(dp[target]) > target) {
-                System.out.println("Impossible");
-            } else {
-                // System.out.println("Minimum cards:");
-                System.out.println(dp[target]);
-                //// Print the different cards used
-                // System.out.println("Cards used:");
-                // int x = target;
-                // while (x > 0) {
-                // for (var c : valuesAvailable) {
-                // if (x - c >= 0 && dp[x] == dp[x - c] + 1) {
-                // System.out.println(c);
-                // x -= c;
-                // break;
-                // }
-                // }
-                // }
-            }
-
+            
+            var result = bottumUp(valuesAvailable, target, false, false);
+            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
         }
-        // Base case is if we need to hit target 0, we can do so with 0 cards.
 
-        // print all DP
-        // For pushing.
-        // for (int i = 0; i < dp.length; i++) {
-        // System.out.println("dp[" + i + "] = " + dp[i]);
-        // }
+        // EVEN: highest amount of cards
+        else if (ruleCard % 2 == 0) {
+            var result = bottumUp(valuesAvailable, target, true, false);
+            System.out.println(result == Integer.MIN_VALUE ?  "Impossible" : result);
 
-        // Print runtime in milliseconds
-        // System.out.println("Runtime: " + (System.nanoTime() - startTime)/1000000 +
-        // "ms");
+        } 
+        scanner.close();
     }
 }
