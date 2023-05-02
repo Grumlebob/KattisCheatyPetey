95cef5018cd3a67555f3071180f67ca191e79912
Jacob Grum <jacg@itu.dk>
2023-05-02 18:07:02 +0200

    new naming

--- submissions/accepted/MainCheatyPetey.java
+++ submissions/accepted/MainCheatyPetey.java
@@ -49,13 +49,15 @@ public class MainCheatyPetey {
             return new MultiSet<T>((HashMap<T, Integer>) ms.clone());
         }
     }
-    
-    private static int bottumUp(MultiSet<Integer> availableValues, int target, boolean worst, boolean oneOfEach) {
+
+    private static int bottomUp(MultiSet<Integer> cardSet, int target, boolean isMostPlays,
+            boolean isPlaysLimited) {
         int[] dp = new int[target + 1];
         @SuppressWarnings("unchecked")
-        MultiSet<Integer>[] valuesAvailableFrom = new MultiSet[target + 1];; //only used if oneOfEach == true
-        for (var i = 0; i < valuesAvailableFrom.length && oneOfEach; i++) {
-            valuesAvailableFrom[i] = availableValues.clone();
+        MultiSet<Integer>[] cardsAvailable = new MultiSet[target + 1];
+        ; // only used if isAmountOfCardsLimited == true
+        for (var i = 0; i < cardsAvailable.length && isPlaysLimited; i++) {
+            cardsAvailable[i] = cardSet.clone();
         }
         dp[0] = 0;
         for (int currentTarget = 1; currentTarget <= target; currentTarget++) {
@@ -65,21 +67,24 @@ public class MainCheatyPetey {
             // low.
             // Because we want to find something where they draw the most cards, and drawing
             // 1 card is more than 0.
-            dp[currentTarget] = worst ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            for (int currentCardValue : availableValues) {
+            dp[currentTarget] = isMostPlays ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int cardValue : cardSet) {
                 // If we don't overdraw.
-                if (currentTarget - currentCardValue < 0 || dp[currentTarget - currentCardValue] == Integer.MAX_VALUE || dp[currentTarget - currentCardValue] == Integer.MIN_VALUE) {
+                if (currentTarget - cardValue < 0 || dp[currentTarget - cardValue] == Integer.MAX_VALUE
+                        || dp[currentTarget - cardValue] == Integer.MIN_VALUE) {
                     continue;
                 }
                 // System.out.println("Current target:" + currentTarget + " Current card value:"
                 // + currentCardValue
                 // + " Current best solution:" + dp[currentTarget] + " New solution:"
                 // + dp[currentTarget - currentCardValue] + 1);
-                if (worst && dp[currentTarget] < dp[currentTarget - currentCardValue] + 1 || !worst && dp[currentTarget] > dp[currentTarget - currentCardValue] + 1) {
-                    dp[currentTarget] = dp[currentTarget - currentCardValue] + 1;
-                    if (oneOfEach) {
-                        valuesAvailableFrom[currentTarget] = valuesAvailableFrom[currentTarget - currentCardValue].clone();
-                        valuesAvailableFrom[currentTarget].remove(currentCardValue);
+                if (isMostPlays && dp[currentTarget] < dp[currentTarget - cardValue] + 1
+                        || !isMostPlays && dp[currentTarget] > dp[currentTarget - cardValue] + 1) {
+                    dp[currentTarget] = dp[currentTarget - cardValue] + 1;
+                    if (isPlaysLimited) {
+                        cardsAvailable[currentTarget] = cardsAvailable[currentTarget - cardValue]
+                                .clone();
+                        cardsAvailable[currentTarget].remove(cardValue);
                     }
                 }
                 // System.out.println(dp[currentTarget]);
@@ -99,53 +104,48 @@ public class MainCheatyPetey {
         int target = ruleCard * 21;
         int numberOfDifferentCardValues = scanner.nextInt();
 
-        MultiSet<Integer> valuesAvailable = new MultiSet<>();
+        MultiSet<Integer> cardSet = new MultiSet<>();
         for (int i = 0; i < numberOfDifferentCardValues; i++) {
-            valuesAvailable.add(scanner.nextInt(), 1);
+            cardSet.add(scanner.nextInt(), 1);
         }
 
-        // System.out.println("target: " + target);
-        // System.out.println("valuesAvailable: " + valuesAvailable);
-
-        // Dynamic programming coin change problem variant, casino themed.
-        
         // Only 1 of each card
         if (ruleCard == 3) {
-            var result = bottumUp(valuesAvailable, target, false, true);
-            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
+            var result = bottomUp(cardSet, target, false, true);
+            System.out.println(result == Integer.MAX_VALUE ? "Impossible" : result);
         }
 
-        // Only 5 of each card 
+        // Only 5 of each card
         else if (ruleCard == 5) {
-            for (var value : valuesAvailable) {
-                valuesAvailable.add(value, 4);
+            for (var value : cardSet) {
+                cardSet.add(value, 4);
             }
-            var result = bottumUp(valuesAvailable, target, false, true);
-            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
+            var result = bottomUp(cardSet, target, false, true);
+            System.out.println(result == Integer.MAX_VALUE ? "Impossible" : result);
         }
 
         // Only 6 of each card and worst possible
         else if (ruleCard == 6) {
-            for (var value : valuesAvailable) {
-                valuesAvailable.add(value, 5);
+            for (var value : cardSet) {
+                cardSet.add(value, 5);
             }
-            var result = bottumUp(valuesAvailable, target, true, true);
-            System.out.println(result == Integer.MIN_VALUE ?  "Impossible" : result);
+            var result = bottomUp(cardSet, target, true, true);
+            System.out.println(result == Integer.MIN_VALUE ? "Impossible" : result);
         }
 
         // ODD: lowest amount of cards
         else if (ruleCard % 2 == 1) {
-            
-            var result = bottumUp(valuesAvailable, target, false, false);
-            System.out.println(result == Integer.MAX_VALUE ?  "Impossible" : result);
+
+            var result = bottomUp(cardSet, target, false, false);
+            System.out.println(result == Integer.MAX_VALUE ? "Impossible" : result);
         }
 
         // EVEN: highest amount of cards
         else if (ruleCard % 2 == 0) {
-            var result = bottumUp(valuesAvailable, target, true, false);
-            System.out.println(result == Integer.MIN_VALUE ?  "Impossible" : result);
+            var result = bottomUp(cardSet, target, true, false);
+            System.out.println(result == Integer.MIN_VALUE ? "Impossible" : result);
 
-        } 
+        }
         scanner.close();
     }
 }
