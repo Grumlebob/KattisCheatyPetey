b7c9e14c570c1629b83908cd36bd7e7a2a57ba26
AGmarson <tcla@itu.dk>
2023-05-09 11:53:45 +0200

    updated dvaput

--- OtherKattisSolutions/dvaput/src/Main.java
+++ OtherKattisSolutions/dvaput/src/Main.java
@@ -1,158 +1,120 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
-    static int[] sa;
-    static int[] lcp;
-    static int maxLCP = 0;
+    static int[] sa; // final suffix array
+    static final int ALPHABET = 27; // BASECHAR + a-z
+    static final char BASECHAR = ('a' - 1); // whatever comes before 'a' (helps sort "a" before "aa" and identifies
+                                            // cycles)
 
     public static void main(String[] args) {
-
-        var scanner = new java.util.Scanner(System.in);
-        scanner.nextInt();
+        var scanner = new Scanner(System.in);
         scanner.nextLine();
         var inputString = scanner.nextLine();
-        sa = suffixArrayConstruction(inputString);
-        lcp = constructLCPArray(inputString, sa);
-        System.out.println(maxLCP);
+        sa = SuffixArrayCyclicShifts(inputString);
+        System.out.println(findMaxLCP(inputString, sa));
+        scanner.close();
     }
 
-    //100 % his solution
-    //https://github.com/stevenhalim/cpbook-code/blob/master/ch6/sa_lcp.py
-    //Sorting with cyclic shifts
-    public static int[] suffixArrayConstruction(String s) {
-        return Arrays.copyOfRange(sortCyclicShifts(s + '\0'), 1, s.length() + 1);
-    }
-    public static int[] sortCyclicShifts(String s) {
+    // Runtime: O(N lgN) with N = string length
+    public static int[] SuffixArrayCyclicShifts(String s) {
+        s = s + BASECHAR;
         int n = s.length();
-        int alphabet = 256;
         int[] suffixArray = new int[n];
         int[] equivalenceClasses = new int[n];
-        int[] charCount = new int[Math.max(alphabet, n)];
+        int[] charCount = new int[Math.max(ALPHABET + 1, n + 1)];
 
-        // Count the number of occurrences of each character in the input string
+        // Count number of occurrences for each character in input string
         for (int i = 0; i < n; i++) {
-            charCount[s.charAt(i)]++;
+            charCount[s.charAt(i) - BASECHAR + 1]++;
         }
-        // Calculate the starting index for each character in the sorted suffix array
-        for (int i = 1; i < alphabet; i++) {
-            charCount[i] += charCount[i-1];
+        // Calculate starting index for each character in the sorted suffix array
+        for (int i = 1; i < ALPHABET; i++) {
+            charCount[i] += charCount[i - 1];
         }
         // Construct the sorted suffix array
-        for (int i = n-1; i >= 0; i--) {
-            charCount[s.charAt(i)]--;
-            suffixArray[charCount[s.charAt(i)]] = i;
+        for (int i = 0; i < n; i++) {
+            suffixArray[charCount[s.charAt(i) - BASECHAR]] = i;
+            charCount[s.charAt(i) - BASECHAR]++;
         }
 
-        // Assign equivalence classes to each suffix
-        equivalenceClasses[suffixArray[0]] = 0;
-        int classes = 1;
+        // Divide suffixes into equivalence classes depending on their first letter
+        int classN = 0;
+        equivalenceClasses[suffixArray[0]] = classN; // first element will always be in the first class
         for (int i = 1; i < n; i++) {
-            if (s.charAt(suffixArray[i]) != s.charAt(suffixArray[i-1])) {
-                classes++;
+            if (s.charAt(suffixArray[i]) != s.charAt(suffixArray[i - 1])) {
+                classN++; // not equal, new class
             }
-            equivalenceClasses[suffixArray[i]] = classes - 1;
+            equivalenceClasses[suffixArray[i]] = classN;
         }
 
-        // Perform multiple rounds of sorting, increasing the suffix size each time
+        // keep sorting after classes. Double suffix size every time
         int[] tempSufixArray = new int[n];
         int[] tempEquivalanceClasses = new int[n];
-        for (int h = 0; (1 << h) < n; h++) {
-
+        for (int shift = 1; shift < n; shift *= 2) {
+            // shift left.
             for (int i = 0; i < n; i++) {
-                tempSufixArray[i] = suffixArray[i] - (1 << h);
-                if (tempSufixArray[i] < 0) {
-                    tempSufixArray[i] += n;
-                }
+                tempSufixArray[i] = (suffixArray[i] - shift + n) % n; // (... + n) % n makes it cycle from -k to n-k instead of going out of bounds
             }
             Arrays.fill(charCount, 0);
             for (int i = 0; i < n; i++) {
-                charCount[equivalenceClasses[tempSufixArray[i]]]++;
+                charCount[equivalenceClasses[tempSufixArray[i]] + 1]++;
             }
-            for (int i = 1; i < classes; i++) {
-                charCount[i] += charCount[i-1];
+            for (int i = 1; i <= classN; i++) {
+                charCount[i] += charCount[i - 1];
             }
 
-            // Construct the sorted suffix array based on the current equivalence classes
-            for (int i = n-1; i >= 0; i--) {
-                charCount[equivalenceClasses[tempSufixArray[i]]]--;
+            // sort after classes (if of same class they dont swap. It's a stable sort)
+            for (int i = 0; i < n; i++) {
                 suffixArray[charCount[equivalenceClasses[tempSufixArray[i]]]] = tempSufixArray[i];
+                charCount[equivalenceClasses[tempSufixArray[i]]]++;
             }
             // Assign new equivalence classes based on the sorted suffix array
-            tempEquivalanceClasses[suffixArray[0]] = 0;
-            classes = 1;
+            classN = 0;
+            tempEquivalanceClasses[suffixArray[0]] = classN; // first element, first class
             for (int i = 1; i < n; i++) {
-                int cur = equivalenceClasses[suffixArray[i]] * alphabet + equivalenceClasses[(suffixArray[i] + (1 << h)) % n];
-                int prev = equivalenceClasses[suffixArray[i-1]] * alphabet + equivalenceClasses[(suffixArray[i-1] + (1 << h)) % n];
+                int cur = equivalenceClasses[suffixArray[i]] * ALPHABET // first half
+                        + equivalenceClasses[(suffixArray[i] + shift) % n]; // second half
+                int prev = equivalenceClasses[suffixArray[i - 1]] * ALPHABET // first half
+                        + equivalenceClasses[(suffixArray[i - 1] + shift) % n]; // second half
                 if (cur != prev) {
-                    classes++;
+                    classN++;
                 }
-                tempEquivalanceClasses[suffixArray[i]] = classes - 1;
+                tempEquivalanceClasses[suffixArray[i]] = classN;
             }
-            // Swap the equivalence class arrays for the next round of sorting
-            int[] temp = equivalenceClasses;
+            // Swap equivalence class arrays to keep the old values for next round
+            var temp = equivalenceClasses;
             equivalenceClasses = tempEquivalanceClasses;
             tempEquivalanceClasses = temp;
         }
-        return suffixArray;
+        return Arrays.copyOfRange(suffixArray, 1, suffixArray.length);
     }
 
-
-    //Runtime: O(N) - N : Length of string
-    public static int[] constructLCPArray(String s, int[] sa) {
-        int n = s.length();
-        int[] lcp = new int[n - 1];
-        int[] invSa = new int[n];
-        maxLCP = 0; // initialize maxLCPFound to 0
-        for (int i = 0; i < n; i++) {
-            invSa[sa[i]] = i;
+    // Runtime: O(N) with N = Length of string
+    public static int findMaxLCP(String s, int[] sa) {
+        int maxLCP = 0;
+        int[] saIndexOf = new int[sa.length];
+        for (int i = 0; i < sa.length; i++) {
+            saIndexOf[sa[i]] = i; // used to find the element in sa before the string of length i
         }
-        int len = 0;
-        char[] chars = s.toCharArray(); // Use char array for indexing
-        for (int i = 0; i < n; i++) {
-            if (invSa[i] == n - 1) {
-                len = 0;
+        int commonChars = 0;
+        // Start from the longest suffix x. Compare it to the element that comes before
+        // x in sa
+        for (int i = 0; i < sa.length; i++) {
+            if (saIndexOf[i] == 0) {
+                commonChars = 0;
                 continue;
             }
-            int j = sa[invSa[i] + 1];
-            int k = i + len;
-            int m = j + len;
-            while (k < n && m < n && chars[k] == chars[m]) { // Use temporary variables for array indexing
-                len++;
-                k++;
-                m++;
-                if (len > maxLCP) { // update maxLCPFound if len is greater
-                    maxLCP = len;
-                }
+            int prev = sa[saIndexOf[i] - 1];
+            boolean withinBounds = i + commonChars < s.length() && prev + commonChars < s.length();
+            while (withinBounds && s.charAt(i + commonChars) == s.charAt(prev + commonChars)) {
+                commonChars++;
+                withinBounds = i + commonChars < s.length() && prev + commonChars < s.length();
             }
-            lcp[invSa[i]] = len;
-            if (len > 0) {
-                len--;
-            }
+            maxLCP = Math.max(maxLCP, commonChars);
+            if (commonChars > 0) commonChars--; // next suffix is one char shorter and has at least as many common chars - 1
         }
-        return lcp;
+        return maxLCP;
     }
-
-
-    //Finds that actual string, that is the longest repeating substring
-    public static String findLongestRepeatingSubstring(String s) {
-        int n = s.length();
-        int maxLcp = 0;
-        int maxLcpIndex = -1;
-        for (int i = 0; i < n - 1; i++) {
-            if ((sa[i] < n / 2 && sa[i + 1] >= n / 2) || (sa[i + 1] < n / 2 && sa[i] >= n / 2)) {
-                if (lcp[i] > maxLcp) {
-                    maxLcp = lcp[i];
-                    maxLcpIndex = i;
-                }
-            }
-        }
-        if (maxLcp > 0) {
-            int startPos = sa[maxLcpIndex];
-            return s.substring(startPos, startPos + maxLcp);
-        } else {
-            return "";
-        }
-    }
-
 }
