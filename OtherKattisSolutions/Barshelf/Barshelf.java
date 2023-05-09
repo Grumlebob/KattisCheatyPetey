f1082ac53133fc29522090bd6449692207cd79c7
Jacob Grum <jacg@itu.dk>
2023-05-09 08:38:02 +0200

    Kattis solutions

---
+++ OtherKattisSolutions/Barshelf/Barshelf.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Barshelf2 {
+    static int n;
+    static int[] segmentTree;
+
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        int[] bottles = new int[n];
+        for (int i = 0; i < n; i++) {
+            bottles[i] = scanner.nextInt();
+        }
+
+        ArrayList<Integer> sortedBottlesWithNeighbours = new ArrayList<>();
+        for (var bottle : bottles) {
+            sortedBottlesWithNeighbours.add(bottle);
+            sortedBottlesWithNeighbours.add(bottle / 2);
+            sortedBottlesWithNeighbours.add(bottle * 2);
+        }
+        Collections.sort(sortedBottlesWithNeighbours);
+
+        HashMap<Integer, Integer> heightToIndex = new HashMap<>();
+        for (int i = 0; i < sortedBottlesWithNeighbours.size(); i++) {
+            heightToIndex.put(sortedBottlesWithNeighbours.get(i), i);
+        }
+
+        segmentTree = new int[2 * sortedBottlesWithNeighbours.size()];
+        long[] messyTrios = new long[n];
+        for (int i = 0; i < n; i++) { //left to right
+            var bottle = bottles[i];
+            add(heightToIndex.get(bottle), 1);
+            messyTrios[i] = sum(heightToIndex.get(bottle * 2), heightToIndex.get(sortedBottlesWithNeighbours.get(sortedBottlesWithNeighbours.size() - 1))); // how many bottles of height >= 2*b have we passed
+        }
+        // now all bottles knows how many previous bottles were at least double height
+
+        segmentTree = new int[2 * sortedBottlesWithNeighbours.size()];
+
+        for (int i = bottles.length - 1; i >= 0; i--) { //right to left
+            var bottle = bottles[i];
+            add(heightToIndex.get(bottle), 1);
+            messyTrios[i] *= sum(1, heightToIndex.get(bottle / 2)); // how many bottles of height <= bottle/2 have we passed
+        }
+
+        System.out.println(Arrays.stream(messyTrios).sum());
+    }
+
+    private static int sum(int from, int to) { //inclusive
+        from += segmentTree.length / 2;
+        to += segmentTree.length / 2;
+        int s = 0;
+        while (from <= to) {
+            if (from % 2 == 1) s += segmentTree[from++];
+            if (to % 2 == 0) s += segmentTree[to--];
+            from /= 2;
+            to /= 2;
+        }
+        return s;
+    }
+
+    private static void add(int index, int amount) {
+        index += segmentTree.length / 2;
+        segmentTree[index] += amount;
+        for (index /= 2; index >= 1; index /= 2) {
+            segmentTree[index] = segmentTree[2 * index] + segmentTree[2 * index + 1];
+        }
+    }
+}
---
+++ OtherKattisSolutions/dvaput/src/Main.java
@@ -0,0 +1,158 @@
+import java.util.*;
+
+public class Main {
+
+    static int[] sa;
+    static int[] lcp;
+    static int maxLCP = 0;
+
+    public static void main(String[] args) {
+
+        var scanner = new java.util.Scanner(System.in);
+        scanner.nextInt();
+        scanner.nextLine();
+        var inputString = scanner.nextLine();
+        sa = suffixArrayConstruction(inputString);
+        lcp = constructLCPArray(inputString, sa);
+        System.out.println(maxLCP);
+    }
+
+    //100 % his solution
+    //https://github.com/stevenhalim/cpbook-code/blob/master/ch6/sa_lcp.py
+    //Sorting with cyclic shifts
+    public static int[] suffixArrayConstruction(String s) {
+        return Arrays.copyOfRange(sortCyclicShifts(s + '\0'), 1, s.length() + 1);
+    }
+    public static int[] sortCyclicShifts(String s) {
+        int n = s.length();
+        int alphabet = 256;
+        int[] suffixArray = new int[n];
+        int[] equivalenceClasses = new int[n];
+        int[] charCount = new int[Math.max(alphabet, n)];
+
+        // Count the number of occurrences of each character in the input string
+        for (int i = 0; i < n; i++) {
+            charCount[s.charAt(i)]++;
+        }
+        // Calculate the starting index for each character in the sorted suffix array
+        for (int i = 1; i < alphabet; i++) {
+            charCount[i] += charCount[i-1];
+        }
+        // Construct the sorted suffix array
+        for (int i = n-1; i >= 0; i--) {
+            charCount[s.charAt(i)]--;
+            suffixArray[charCount[s.charAt(i)]] = i;
+        }
+
+        // Assign equivalence classes to each suffix
+        equivalenceClasses[suffixArray[0]] = 0;
+        int classes = 1;
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(suffixArray[i]) != s.charAt(suffixArray[i-1])) {
+                classes++;
+            }
+            equivalenceClasses[suffixArray[i]] = classes - 1;
+        }
+
+        // Perform multiple rounds of sorting, increasing the suffix size each time
+        int[] tempSufixArray = new int[n];
+        int[] tempEquivalanceClasses = new int[n];
+        for (int h = 0; (1 << h) < n; h++) {
+
+            for (int i = 0; i < n; i++) {
+                tempSufixArray[i] = suffixArray[i] - (1 << h);
+                if (tempSufixArray[i] < 0) {
+                    tempSufixArray[i] += n;
+                }
+            }
+            Arrays.fill(charCount, 0);
+            for (int i = 0; i < n; i++) {
+                charCount[equivalenceClasses[tempSufixArray[i]]]++;
+            }
+            for (int i = 1; i < classes; i++) {
+                charCount[i] += charCount[i-1];
+            }
+
+            // Construct the sorted suffix array based on the current equivalence classes
+            for (int i = n-1; i >= 0; i--) {
+                charCount[equivalenceClasses[tempSufixArray[i]]]--;
+                suffixArray[charCount[equivalenceClasses[tempSufixArray[i]]]] = tempSufixArray[i];
+            }
+            // Assign new equivalence classes based on the sorted suffix array
+            tempEquivalanceClasses[suffixArray[0]] = 0;
+            classes = 1;
+            for (int i = 1; i < n; i++) {
+                int cur = equivalenceClasses[suffixArray[i]] * alphabet + equivalenceClasses[(suffixArray[i] + (1 << h)) % n];
+                int prev = equivalenceClasses[suffixArray[i-1]] * alphabet + equivalenceClasses[(suffixArray[i-1] + (1 << h)) % n];
+                if (cur != prev) {
+                    classes++;
+                }
+                tempEquivalanceClasses[suffixArray[i]] = classes - 1;
+            }
+            // Swap the equivalence class arrays for the next round of sorting
+            int[] temp = equivalenceClasses;
+            equivalenceClasses = tempEquivalanceClasses;
+            tempEquivalanceClasses = temp;
+        }
+        return suffixArray;
+    }
+
+
+    //Runtime: O(N) - N : Length of string
+    public static int[] constructLCPArray(String s, int[] sa) {
+        int n = s.length();
+        int[] lcp = new int[n - 1];
+        int[] invSa = new int[n];
+        maxLCP = 0; // initialize maxLCPFound to 0
+        for (int i = 0; i < n; i++) {
+            invSa[sa[i]] = i;
+        }
+        int len = 0;
+        char[] chars = s.toCharArray(); // Use char array for indexing
+        for (int i = 0; i < n; i++) {
+            if (invSa[i] == n - 1) {
+                len = 0;
+                continue;
+            }
+            int j = sa[invSa[i] + 1];
+            int k = i + len;
+            int m = j + len;
+            while (k < n && m < n && chars[k] == chars[m]) { // Use temporary variables for array indexing
+                len++;
+                k++;
+                m++;
+                if (len > maxLCP) { // update maxLCPFound if len is greater
+                    maxLCP = len;
+                }
+            }
+            lcp[invSa[i]] = len;
+            if (len > 0) {
+                len--;
+            }
+        }
+        return lcp;
+    }
+
+
+    //Finds that actual string, that is the longest repeating substring
+    public static String findLongestRepeatingSubstring(String s) {
+        int n = s.length();
+        int maxLcp = 0;
+        int maxLcpIndex = -1;
+        for (int i = 0; i < n - 1; i++) {
+            if ((sa[i] < n / 2 && sa[i + 1] >= n / 2) || (sa[i + 1] < n / 2 && sa[i] >= n / 2)) {
+                if (lcp[i] > maxLcp) {
+                    maxLcp = lcp[i];
+                    maxLcpIndex = i;
+                }
+            }
+        }
+        if (maxLcp > 0) {
+            int startPos = sa[maxLcpIndex];
+            return s.substring(startPos, startPos + maxLcp);
+        } else {
+            return "";
+        }
+    }
+
+}
