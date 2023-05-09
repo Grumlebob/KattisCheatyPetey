abf4011f223e8ac5898fde55f81d9cd401923f49
AGmarson <tcla@itu.dk>
2023-05-09 11:31:02 +0200

    add dutyscheduler

---
+++ OtherKattisSolutions/DutyScheduler/src/FordFulkerson.java
@@ -0,0 +1,86 @@
+public class FordFulkerson {
+    private final int V;          // number of vertices
+    private boolean[] marked;     // marked[v] = true iff s->v path in residual graph
+    private FlowEdge[] edgeTo;    // edgeTo[v] = last edge on shortest residual s->v path
+    private int value;         // current value of max flow
+
+
+    public FordFulkerson(FlowNetwork G, int s, int t) {
+        V = G.V();
+        validate(s);
+        validate(t);
+        if (s == t)               throw new IllegalArgumentException("Source equals sink");
+
+        // while there exists an augmenting path, use it
+        value = excess(G, t);
+        while (hasAugmentingPath(G, s, t)) {
+
+            // compute bottleneck capacity
+            double bottle = Double.POSITIVE_INFINITY;
+            for (int v = t; v != s; v = edgeTo[v].other(v)) {
+                bottle = Math.min(bottle, edgeTo[v].residualCapacityTo(v));
+            }
+
+            // augment flow
+            for (int v = t; v != s; v = edgeTo[v].other(v)) {
+                edgeTo[v].addResidualFlowTo(v, bottle);
+            }
+
+            value += bottle;
+        }
+
+    }
+
+
+    public int value()  {
+        return value;
+    }
+
+
+    public boolean inCut(int v)  {
+        validate(v);
+        return marked[v];
+    }
+
+    private void validate(int v)  {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    private boolean hasAugmentingPath(FlowNetwork G, int s, int t) {
+        edgeTo = new FlowEdge[G.V()];
+        marked = new boolean[G.V()];
+
+        // breadth-first search
+        Queue<Integer> queue = new Queue<Integer>();
+        queue.enqueue(s);
+        marked[s] = true;
+        while (!queue.isEmpty() && !marked[t]) {
+            int v = queue.dequeue();
+
+            for (FlowEdge e : G.adj(v)) {
+                int w = e.other(v);
+
+                // if residual capacity from v to w
+                if (e.residualCapacityTo(w) > 0) {
+                    if (!marked[w]) {
+                        edgeTo[w] = e;
+                        marked[w] = true;
+                        queue.enqueue(w);
+                    }
+                }
+            }
+        }
+
+        return marked[t];
+    }
+
+    private int excess(FlowNetwork G, int v) {
+        int excess = 0;
+        for (FlowEdge e : G.adj(v)) {
+            if (v == e.from()) excess -= e.flow();
+            else               excess += e.flow();
+        }
+        return excess;
+    }
+}
